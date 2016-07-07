6dbf5b78f3984455f9fb239137a761d408e3d1fb
Roman <kaboom.ua@gmail.com>
2016-07-06 18:07:43 -0700

    Update Comment.js

--- src/Comment.js
+++ src/Comment.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 class Comment extends Component {
     render() {
         const {comment} = this.props
+        //лучше сделать Comment stateless-компонентом + не вносить li, а то сильно ограничиваете возможность переиспользовать компонент
         return (
             <li>
                 <p>{comment.text}</p>
