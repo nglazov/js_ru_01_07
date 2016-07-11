bffc1e0533cf33491e891bfe4296aea117ba9a0b
Glazov Nikita <nikita.glazov@smartbics.com>
2016-07-11 16:15:51 +0300

    Use stateless function

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,26 +1,24 @@
-import React, { Component }  from 'react'
+import React, {Component}  from 'react'
 import Article from './Article'
 import oneOpenArticle from './decorators/oneOpenArticle'
 
-class ArticleList extends Component {
-    render() {
-        const { articles, openArticle, openArticleId } = this.props
+function ArticleList(props) {
+    const {articles, openArticle, openArticleId} = props
 
-        const listItems = articles.map((article) => <li key={article.id}>
-            <Article article = {article}
-                isOpen = {article.id === openArticleId}
-                openArticle = { openArticle(article.id) }
-            />
-        </li>)
-        return (
-            <div>
-                <h1>Article list</h1>
-                <ul>
-                    {listItems}
-                </ul>
-            </div>
-        )
-    }
+    const listItems = articles.map((article) => <li key={article.id}>
+        <Article article={article}
+                 isOpen={article.id === openArticleId}
+                 openArticle={ openArticle(article.id) }
+        />
+    </li>)
+    return (
+        <div>
+            <h1>Article list</h1>
+            <ul>
+                {listItems}
+            </ul>
+        </div>
+    )
 }
 
 export default oneOpenArticle(ArticleList)
