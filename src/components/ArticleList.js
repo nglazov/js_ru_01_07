1a265f10b651d4324a33e29504b5ce5f28ce61c2
Glazov Nikita <nikita.glazov@smartbics.com>
2016-07-14 18:55:21 +0300

    hometask #3

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,14 +4,24 @@ import oneOpen from '../decorators/oneOpen'
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
 
+import DayPicker, { DateUtils } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
+
+import moment from 'moment';
+
 class ArticleList extends Component {
 
     state = {
-        selectedArticles: null
+        selectedArticles: null,
+        range            : {
+            from: null,
+            to  : null
+        }
     }
 
     render() {
         const { articles, isItemOpen, toggleOpenItem } = this.props
+        const {from, to} = this.state.range
 
         const listItems = articles.map((article) => <li key={article.id}>
             <Article article = {article}
@@ -33,12 +43,25 @@ class ArticleList extends Component {
                     value = {this.state.selectedArticles}
                     onChange = {this.handleSelectChange}
                 />
+
+                <DayPicker
+                    selectedDays={day => DateUtils.isDayInRange(day, { from, to })}
+                    onDayClick={this.handleDayClick}
+                />
+
+                <p>from {moment(from).format('L')} to {moment(to).format('L')}</p>
+
                 <ul>
                     {listItems}
                 </ul>
             </div>
         )
     }
+
+    handleDayClick = ((e, day) => {
+        const range = DateUtils.addDayToRange(day, this.state.range);
+        this.setState({range});
+    }).bind(this)
 
     handleSelectChange = (selectedArticles) => {
         console.log(selectedArticles)
