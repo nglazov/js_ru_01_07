d1e4a659581b4597d3baba96bd795badf2f89d65
Roman <kaboom.ua@gmail.com>
2016-07-25 22:48:33 +0300

    Update addComment.js

--- src/middlewares/addComment.js
+++ src/middlewares/addComment.js
@@ -1,8 +1,11 @@
 import {ADD_COMMENT} from '../constants'
 
 export default store => next => action => {
+    //мидлвары лучше делать универсальными, что б можно было переиспользовать эту логику
     if (action.type === ADD_COMMENT) {
         //нужно ли здесь использовать имьютабл?
+        //иммютабл использовать не стоит, но и экшн менять - плохая практика, лучше сделть newAction = {...action, randomId}
+        //а вот так генерировать id - плохо, никаких гарантий уникальности совсем
         action.payload.id = store.getState().comments.last().id + 1
         next(action)
     }
